41b3840899f3f69b450e38b92fc8214ea4e6ec38
TakeshiMusgrave <tkm45@cornell.edu>
2021-09-01 07:22:50 -0400

    Updated test_neighborhood_aggregation. Made NeighborhoodAggregation use torch.mm instead of faiss due to complicated issue with ignoring previous self in memory

--- tests/layers/test_neighborhood_aggregation.py
+++ tests/layers/test_neighborhood_aggregation.py
@@ -23,6 +23,23 @@ def get_correct_pseudo_labels(features, feat_memory, pred_memory, idx, k):
     return torch.max(w.mm(pred_memory), 1)
 
 
+# from https://github.com/tim-learn/ATDOC/blob/main/demo_uda.py
+def get_correct_memory_update(
+    features_target, outputs_target, mem_fea, mem_cls, idx, momentum=1
+):
+    features_target = features_target / torch.norm(
+        features_target, p=2, dim=1, keepdim=True
+    )
+    softmax_out = torch.nn.Softmax(dim=1)(outputs_target)
+
+    # https://github.com/tim-learn/ATDOC/issues/1
+    outputs_target = softmax_out ** 2 / ((softmax_out ** 2).sum(dim=1, keepdims=True))
+
+    mem_fea[idx] = (1.0 - momentum) * mem_fea[idx] + momentum * features_target.clone()
+    mem_cls[idx] = (1.0 - momentum) * mem_cls[idx] + momentum * outputs_target.clone()
+    return mem_fea, mem_cls
+
+
 class TestNeighborhoodAggregation(unittest.TestCase):
     def test_neighborhood_aggregation(self):
         dataset_size = 100000
@@ -30,23 +47,37 @@ class TestNeighborhoodAggregation(unittest.TestCase):
         num_classes = 123
         k = 5
 
-        if TEST_DEVICE.type == "cuda":
-            na_device = torch.device("cuda:0")
         na = NeighborhoodAggregation(dataset_size, feature_dim, num_classes, k=k).to(
-            na_device
+            TEST_DEVICE
         )
         weighter = ConfidenceWeights()
 
         batch_size = 64
-        features = torch.randn(batch_size, feature_dim, device=TEST_DEVICE)
-        logits = torch.randn(batch_size, num_classes, device=TEST_DEVICE)
-        idx = torch.randint(0, dataset_size, size=(64,))
+
         for i in range(10):
+            features = torch.randn(batch_size, feature_dim, device=TEST_DEVICE)
+            logits = torch.randn(batch_size, num_classes, device=TEST_DEVICE)
+            idx = torch.randint(0, dataset_size, size=(64,))
+
+            curr_feat_memory = na.feat_memory.clone()
+            curr_pred_memory = na.pred_memory.clone()
+
             pseudo_labels, neighbor_logits = na(features, logits, update=True, idx=idx)
             weights = weighter(neighbor_logits)
-            if i > 0:
-                correct_weights, correct_pseudo_labels = get_correct_pseudo_labels(
-                    features, na.feat_memory, na.pred_memory, idx, k
-                )
-                self.assertTrue(torch.equal(pseudo_labels, correct_pseudo_labels))
-                self.assertTrue(torch.allclose(weights, correct_weights))
+
+            self.assertTrue(not torch.allclose(curr_feat_memory, na.feat_memory))
+            self.assertTrue(not torch.allclose(curr_pred_memory, na.pred_memory))
+
+            correct_weights, correct_pseudo_labels = get_correct_pseudo_labels(
+                features, curr_feat_memory, curr_pred_memory, idx, k
+            )
+
+            self.assertTrue(torch.equal(pseudo_labels, correct_pseudo_labels))
+            self.assertTrue(torch.allclose(weights, correct_weights))
+
+            curr_feat_memory, curr_pred_memory = get_correct_memory_update(
+                features, logits, curr_feat_memory, curr_pred_memory, idx
+            )
+
+            self.assertTrue(torch.allclose(curr_feat_memory, na.feat_memory))
+            self.assertTrue(torch.allclose(curr_pred_memory, na.pred_memory))
