fd8f95d96eff738b3bdb39455127986655c8225b
KevinMusgrave <tkm45@cornell.edu>
2021-09-02 18:03:21 -0400

    More docs for layers

--- docs/layers/index.md
+++ docs/layers/index.md
@@ -2,3 +2,7 @@
 ::: pytorch_adapt.layers.adaptive_feature_norm
 ::: pytorch_adapt.layers.batch_spectral_loss
 ::: pytorch_adapt.layers.bnm_loss
+::: pytorch_adapt.layers.concat_softmax
+::: pytorch_adapt.layers.confidence_weights
+::: pytorch_adapt.layers.coral_loss
+::: pytorch_adapt.layers.diversity_loss
--- src/pytorch_adapt/layers/confidence_weights.py
+++ src/pytorch_adapt/layers/confidence_weights.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import torch
 
 from ..utils import common_functions as c_f
@@ -5,9 +7,23 @@ from .normalizers import NoNormalizer
 
 
 class ConfidenceWeights(torch.nn.Module):
-    def __init__(self, normalizer=None):
+    """
+    Returns the max value along each row of the input,
+    followed by an optional normalization function.
+    The output of this can be used to weight
+    classification losses by the "confidence" of the predictions.
+    """
+
+    def __init__(self, normalizer: Callable[[torch.Tensor], torch.Tensor] = None):
+        """
+        Arguments:
+            normalizer: A callable for normalizing
+                (e.g. min-max normalization) the weights.
+        """
+
         super().__init__()
         self.normalizer = c_f.default(normalizer, NoNormalizer())
 
     def forward(self, logits):
+        """"""
         return self.normalizer(torch.max(logits, dim=1)[0])
