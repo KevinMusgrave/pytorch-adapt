9334c5e717259720d9c1ddb566a888ebee53d50a
KevinMusgrave <tkm45@cornell.edu>
2021-09-13 13:51:51 -0400

    Add pretrained option to hubconfg

--- hubconf.py
+++ hubconf.py
@@ -6,16 +6,17 @@ import torch
 from pytorch_adapt.models import MNISTFeatures
 
 
-def mnist(model_name):
+def mnist(name, pretrained=True):
     url = {
         "C": "https://cornell.box.com/shared/static/j4zrogronmievq1csulrkai7zjm27gcq",
         "G": "https://cornell.box.com/shared/static/tdx0ts24e273j7mf3r2ox7a12xh4fdfy",
-    }[model_name]
+    }[name]
 
-    if model_name == "C":
+    if name == "C":
         model = Classifier(num_classes=10, in_size=1200, h=256)
-    elif model_name == "G":
+    elif name == "G":
         model = MNISTFeatures()
 
-    model.load_state_dict(torch.hub.load_state_dict_from_url(url, progress=True))
+    if pretrained:
+        model.load_state_dict(torch.hub.load_state_dict_from_url(url, progress=True))
     return model
