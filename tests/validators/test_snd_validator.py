d33457c09ccf7eb0bc709a4b64a9e73b66d97ad4
TakeshiMusgrave <tkm45@cornell.edu>
2021-09-22 22:32:26 -0400

    Added more to test_snd_validator

--- tests/validators/test_snd_validator.py
+++ tests/validators/test_snd_validator.py
@@ -3,10 +3,31 @@ import unittest
 import numpy as np
 import torch
 import torch.nn.functional as F
+from pytorch_metric_learning.utils import common_functions as pml_cf
 
+from pytorch_adapt.adapters import Finetuner
+from pytorch_adapt.containers import Models, Optimizers
+from pytorch_adapt.datasets import (
+    CombinedSourceAndTargetDataset,
+    SourceDataset,
+    TargetDataset,
+)
+from pytorch_adapt.frameworks import Ignite
 from pytorch_adapt.validators import SNDValidator
 
 from .. import TEST_DEVICE
+
+
+def simple_compute_snd(logits, T):
+    logits = F.softmax(logits, dim=1)
+    logits = F.normalize(logits, dim=1)
+    sim_mat = torch.mm(logits, logits.t())
+    n = sim_mat.shape[0]
+    mask = torch.eye(n, dtype=torch.bool)
+    sim_mat = sim_mat[~mask].view(n, n - 1)
+    sim_mat = F.softmax(sim_mat / T, dim=1)
+    entropies = -torch.sum(sim_mat * torch.log(sim_mat), dim=1)
+    return torch.mean(entropies).item()
 
 
 class TestSNDValidator(unittest.TestCase):
@@ -14,7 +35,7 @@ class TestSNDValidator(unittest.TestCase):
         ignore_epoch = 0
         for T in [0.01, 0.05, 0.5, 1]:
             for batch_size in [33, 257, 999, 1000, 1001]:
-                for dataset_size in [100, 1000, 10000]:
+                for dataset_size in [10, 100, 1000, 10000]:
                     for num_classes in [13, 23, 98]:
                         validator = SNDValidator(
                             T=T, batch_size=batch_size, ignore_epoch=ignore_epoch
@@ -24,22 +45,11 @@ class TestSNDValidator(unittest.TestCase):
                             logits = torch.randn(
                                 dataset_size, num_classes, device=TEST_DEVICE
                             )
-                            logits = F.softmax(logits, dim=1)
-                            target_train = {"preds": logits}
-
+                            target_train = {"preds": F.softmax(logits, dim=1)}
                             score = validator.score(
                                 epoch=epoch, target_train=target_train
                             )
-
-                            logits = F.normalize(logits, dim=1)
-                            sim_mat = torch.mm(logits, logits.t())
-                            n = sim_mat.shape[0]
-                            mask = torch.eye(n, dtype=torch.bool)
-                            sim_mat = sim_mat[~mask].view(n, n - 1)
-                            sim_mat = F.softmax(sim_mat / T, dim=1)
-                            entropies = -torch.sum(sim_mat * torch.log(sim_mat), dim=1)
-                            correct_score = torch.mean(entropies).item()
-
+                            correct_score = simple_compute_snd(logits, T)
                             self.assertTrue(np.isclose(score, correct_score))
 
                             if epoch != ignore_epoch:
@@ -49,3 +59,35 @@ class TestSNDValidator(unittest.TestCase):
                         self.assertTrue(
                             np.isclose(validator.best_score, max(all_scores))
                         )
+
+    def test_snd_validator_with_framework(self):
+        dataset_size = 9999
+
+        train_datasets = []
+        all_features = []
+        for _ in range(3):
+            features = torch.randn(dataset_size, 128)
+            labels = torch.randint(0, 10, size=(dataset_size,))
+            all_features.append(features)
+            train_datasets.append(pml_cf.EmbeddingDataset(features, labels))
+
+        train_dataset = CombinedSourceAndTargetDataset(
+            SourceDataset(train_datasets[0]), TargetDataset(train_datasets[1])
+        )
+        target_train = TargetDataset(train_datasets[2])
+
+        C = torch.nn.Linear(128, 10)
+        models = Models({"G": torch.nn.Identity(), "C": C})
+        optimizers = Optimizers((torch.optim.Adam, {"lr": 0}))
+        adapter = Ignite(Finetuner(models=models, optimizers=optimizers))
+        score, _ = adapter.run(
+            {"train": train_dataset, "target_train": target_train},
+            validator=SNDValidator(),
+            epoch_length=1,
+        )
+
+        with torch.no_grad():
+            logits = C(all_features[2].to(TEST_DEVICE))
+
+        correct_score = simple_compute_snd(logits, T=0.05)
+        self.assertTrue(np.isclose(score, correct_score))
