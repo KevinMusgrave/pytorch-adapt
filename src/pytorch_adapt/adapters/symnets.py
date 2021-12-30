5885f12b9d44ab15836b9d302cca2865992d1533
KevinMusgrave <tkm45@cornell.edu>
2021-12-30 15:31:51 -0500

    Updated docs for SymNets adapter

--- src/pytorch_adapt/adapters/symnets.py
+++ src/pytorch_adapt/adapters/symnets.py
@@ -1,3 +1,7 @@
+from typing import Tuple
+
+import torch
+
 from ..hooks import SymNetsHook
 from ..utils.common_functions import check_domain
 from .base_adapter import BaseGCAdapter
@@ -6,12 +10,27 @@ from .utils import with_opt
 
 class SymNets(BaseGCAdapter):
     """
-    Wraps [SymNetsHook][pytorch_adapt.hooks.symnets].
+    Extends [BaseGCAdapter][pytorch_adapt.adapters.base_adapter.BaseGCAdapter]
+    and wraps [SymNetsHook][pytorch_adapt.hooks.SymNetsHook].
+
+    |Container|Required keys|
+    |---|---|
+    |models|```["G", "C"]```|
+    |optimizers|```["G", "C"]```|
+
+    The C model must output a list of logits: ```[logits1, logits2]```.
     """
 
     hook_cls = SymNetsHook
 
-    def inference_default(self, x, domain):
+    def inference_default(self, x, domain) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Arguments:
+            x: The input to the model
+            domain: 0 for the source domain, 1 for the target domain.
+        Returns:
+            Features and logits, where ```logits = C(features)[domain]```.
+        """
         domain = check_domain(self, domain)
         features = self.models["G"](x)
         logits = self.models["C"](features)[domain]
