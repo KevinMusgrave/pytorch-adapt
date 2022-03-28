27f1fbaf7ac7060ca3be05bb77d094a62d793586
KevinMusgrave <tkm45@cornell.edu>
2022-03-28 07:00:57 -0400

    Don't add index.md files automatically

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -13,11 +13,7 @@ for path in sorted(Path("src").rglob("*.py")):
 
     parts = list(module_path.parts)
 
-    if parts[-1] == "__init__":
-        parts = parts[:-1]
-        doc_path = doc_path.with_name("index.md")
-        full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1] == "__main__":
+    if parts[-1] in ["__init__", "__main__"]:
         continue
 
     nav[parts] = doc_path
@@ -28,5 +24,5 @@ for path in sorted(Path("src").rglob("*.py")):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:  # (2)
-    nav_file.writelines(nav.build_literate_nav())  # (3)
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
