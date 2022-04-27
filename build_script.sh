e690f79ea96a645885c5f0a3c26505b7d56c5894
KevinMusgrave <tkm45@cornell.edu>
2022-04-27 15:35:45 -0400

    Set RUN_DATASET_TESTS=true in build_script

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,5 @@
 ./format_code.sh
-python -m unittest discover && \
+RUN_DATASET_TESTS=true python -m unittest discover && \
 rm -rfv build/ && \
 rm -rfv dist/ && \
 rm -rfv src/pytorch_adapt.egg-info/ && \
