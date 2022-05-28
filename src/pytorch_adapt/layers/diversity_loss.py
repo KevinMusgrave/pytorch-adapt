Fix softmax bug in DiversityLoss

src/pytorch_adapt/layers/diversity_loss.py
@@ -27,8 +27,10 @@ class DiversityLoss(torch.nn.Module):
 
     def forward(self, logits):
         """"""
+        if not self.after_softmax:
+            logits = torch.softmax(logits, dim=1)
         logits = torch.mean(logits, dim=0, keepdim=True)
-        return -torch.mean(get_entropy(logits, self.after_softmax))
+        return -torch.mean(get_entropy(logits, after_softmax=True))
 
     def extra_repr(self):
         """"""

tests/layers/test_diversity_loss.py
@@ -10,16 +10,17 @@ from .. import TEST_DEVICE, TEST_DTYPES
 class TestDiversityLoss(unittest.TestCase):
     def test_diversity_loss(self):
         loss_fn1 = DiversityLoss()
-        loss_fn2 = EntropyLoss()
+        loss_fn2 = EntropyLoss(after_softmax=True)
         for dtype in TEST_DTYPES:
             for batch_size in [1, 32]:
                 embedding_size = 100
-                x = torch.randn(
+                logits = torch.randn(
                     batch_size, embedding_size, device=TEST_DEVICE, requires_grad=True
                 ).type(dtype)
-                loss1 = loss_fn1(x)
+                loss1 = loss_fn1(logits)
+                preds = torch.softmax(logits, dim=1)
                 if batch_size == 1:
-                    loss2 = loss_fn2(x)
+                    loss2 = loss_fn2(preds)
                 else:
-                    loss2 = loss_fn2(torch.mean(x, dim=0).unsqueeze(0))
-                self.assertTrue(torch.isclose(loss1, -loss2, rtol=1e-2))
+                    loss2 = loss_fn2(torch.mean(preds, dim=0).unsqueeze(0))
+                self.assertTrue(torch.isclose(loss1, -loss2))