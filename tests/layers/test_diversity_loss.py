8b8f3bd9aa6a028f42cb58efc5f0abf5183e0bca
KevinMusgrave <tkm45@cornell.edu>
2022-05-27 21:35:50 -0400

    Improved tests related to DiversityLoss

--- tests/layers/test_diversity_loss.py
+++ tests/layers/test_diversity_loss.py
@@ -9,18 +9,25 @@ from .. import TEST_DEVICE, TEST_DTYPES
 
 class TestDiversityLoss(unittest.TestCase):
     def test_diversity_loss(self):
-        loss_fn1 = DiversityLoss()
         loss_fn2 = EntropyLoss(after_softmax=True)
-        for dtype in TEST_DTYPES:
-            for batch_size in [1, 32]:
-                embedding_size = 100
-                logits = torch.randn(
-                    batch_size, embedding_size, device=TEST_DEVICE, requires_grad=True
-                ).type(dtype)
-                loss1 = loss_fn1(logits)
-                preds = torch.softmax(logits, dim=1)
-                if batch_size == 1:
-                    loss2 = loss_fn2(preds)
-                else:
-                    loss2 = loss_fn2(torch.mean(preds, dim=0).unsqueeze(0))
-                self.assertTrue(torch.isclose(loss1, -loss2))
+        for after_softmax in [False, True]:
+            loss_fn1 = DiversityLoss(after_softmax=after_softmax)
+            for dtype in TEST_DTYPES:
+                for batch_size in [1, 32]:
+                    embedding_size = 100
+                    logits = torch.randn(
+                        batch_size,
+                        embedding_size,
+                        device=TEST_DEVICE,
+                        requires_grad=True,
+                    ).type(dtype)
+                    preds = torch.softmax(logits, dim=1)
+                    if after_softmax:
+                        loss1 = loss_fn1(preds)
+                    else:
+                        loss1 = loss_fn1(logits)
+                    if batch_size == 1:
+                        loss2 = loss_fn2(preds)
+                    else:
+                        loss2 = loss_fn2(torch.mean(preds, dim=0).unsqueeze(0))
+                    self.assertTrue(torch.isclose(loss1, -loss2))
