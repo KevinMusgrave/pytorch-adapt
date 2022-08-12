a3d64a7451bacf954efe0ac378ecbb9381e3d513
KevinMusgrave <tkm45@cornell.edu>
2022-08-12 19:28:47 -0400

    Test that average=macro is equivalent to sklearn balanced_accuracy_score

--- tests/validators/test_accuracy_validator.py
+++ tests/validators/test_accuracy_validator.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 import torch
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
 
 from pytorch_adapt.validators import AccuracyValidator, ScoreHistory
 
@@ -14,54 +14,77 @@ class TestAccuracyValidator(unittest.TestCase):
 
         for start in [-1, 0, 1]:
             for step in [1, 2]:
-                validator1 = ScoreHistory(
-                    AccuracyValidator(), ignore_epoch=ignore_epoch
-                )
-                validator2 = ScoreHistory(
-                    AccuracyValidator(layer="logits"), ignore_epoch=ignore_epoch
-                )
-                correct_scores = []
-                for i, epoch in enumerate(range(start, 5, step)):
-                    labels = torch.randint(0, 10, (dataset_size,))
-                    logits = torch.randn(dataset_size, 10)
-                    preds = torch.softmax(logits, dim=1)
-                    score1 = validator1(
-                        epoch=epoch, src_val={"labels": labels, "preds": preds}
+                for torchmetric_kwargs in [
+                    None,
+                    {"average": "macro", "num_classes": 10},
+                ]:
+                    validator1 = ScoreHistory(
+                        AccuracyValidator(torchmetric_kwargs=torchmetric_kwargs),
+                        ignore_epoch=ignore_epoch,
                     )
-                    score2 = validator2(
-                        epoch=epoch, src_val={"labels": labels, "logits": logits}
+                    validator2 = ScoreHistory(
+                        AccuracyValidator(
+                            layer="logits", torchmetric_kwargs=torchmetric_kwargs
+                        ),
+                        ignore_epoch=ignore_epoch,
                     )
-                    correct_score = accuracy_score(
-                        labels.numpy(), np.argmax(logits.numpy(), axis=1)
-                    )
+                    correct_scores = []
+                    for i, epoch in enumerate(range(start, 5, step)):
+                        labels = torch.randint(0, 10, (dataset_size,))
+                        labels[:900] = 0  # make it unbalanced
+                        logits = torch.randn(dataset_size, 10)
+                        preds = torch.softmax(logits, dim=1)
+                        score1 = validator1(
+                            epoch=epoch, src_val={"labels": labels, "preds": preds}
+                        )
+                        score2 = validator2(
+                            epoch=epoch, src_val={"labels": labels, "logits": logits}
+                        )
 
-                    if epoch != ignore_epoch:
-                        correct_scores.append(correct_score)
+                        np_argmax = np.argmax(logits.numpy(), axis=1)
 
-                    for validator, score in [
-                        (validator1, score1),
-                        (validator2, score2),
-                    ]:
+                        sklearn_score1 = accuracy_score(labels.numpy(), np_argmax)
+                        sklearn_score2 = balanced_accuracy_score(
+                            labels.numpy(), np_argmax
+                        )
+                        correct_score = (
+                            sklearn_score1
+                            if torchmetric_kwargs is None
+                            else sklearn_score2
+                        )
+                        self.assertTrue(sklearn_score1 != sklearn_score2)
+
                         if epoch != ignore_epoch:
-                            self.assertTrue(
-                                validator.score_history_ignore_epoch[validator.best_idx]
-                                == validator.best_score
-                            )
-                            self.assertTrue(
-                                validator.epochs_ignore_epoch[validator.best_idx]
-                                == validator.best_epoch
-                            )
-                            self.assertTrue(
-                                np.isclose(validator.best_score, max(correct_scores))
-                            )
-                            self.assertTrue(
-                                np.isclose(
-                                    validator.best_idx, np.argmax(correct_scores)
-                                )
-                            )
-                        elif i == 0 and epoch == ignore_epoch:
-                            self.assertTrue(validator.best_epoch is None)
-                            self.assertTrue(validator.best_score is None)
+                            correct_scores.append(correct_score)
 
-                        self.assertTrue(np.isclose(score, correct_score))
-                        self.assertTrue(validator.latest_score == score)
+                        for validator, score in [
+                            (validator1, score1),
+                            (validator2, score2),
+                        ]:
+                            if epoch != ignore_epoch:
+                                self.assertTrue(
+                                    validator.score_history_ignore_epoch[
+                                        validator.best_idx
+                                    ]
+                                    == validator.best_score
+                                )
+                                self.assertTrue(
+                                    validator.epochs_ignore_epoch[validator.best_idx]
+                                    == validator.best_epoch
+                                )
+                                self.assertTrue(
+                                    np.isclose(
+                                        validator.best_score, max(correct_scores)
+                                    )
+                                )
+                                self.assertTrue(
+                                    np.isclose(
+                                        validator.best_idx, np.argmax(correct_scores)
+                                    )
+                                )
+                            elif i == 0 and epoch == ignore_epoch:
+                                self.assertTrue(validator.best_epoch is None)
+                                self.assertTrue(validator.best_score is None)
+
+                            self.assertTrue(np.isclose(score, correct_score))
+                            self.assertTrue(validator.latest_score == score)
